84389c20ca7af452ab4074ddbb5e675c21983b80
Peter Yeung <peteryeung0530@gmail.com>
2013-11-29 08:46:01 -0800

    questions now points of the right json

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,7 @@ class StaticPagesController < ApplicationController
   end
 
   def questions
-  	@file = File.read("#{Rails.root}/app/assets/Seeds.json")
+  	@file = File.read("#{Rails.root}/app/assets/Questions.json")
   	render :json => @file
   end
   
