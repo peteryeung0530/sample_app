30087d7b1b42129912a69cdba477a0a1f67339c4
Peter Yeung <peteryeung0530@gmail.com>
2014-01-19 22:40:11 -0800

    Finish layout and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,25 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
-  	
   end
 
   def help
   end
 
-  def seeds
-  	@file = File.read("#{Rails.root}/app/assets/Seeds.json")
-  	render :json => @file
+  def contact
   end
 
-  def questions
-  	@file = File.read("#{Rails.root}/app/assets/Questions.json")
-  	render :json => @file
-  end
-
-  def solutions
-    @file = File.read("#{Rails.root}/app/assets/Solution.json")
-    render :json => @file
-  end
-
-  
 end
